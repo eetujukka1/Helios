da397d01e6ecc92dfa75816cec8a54d9a89ebf80
eetujukka1 <142122947+eetujukka1@users.noreply.github.com>
2026-05-09 15:04:05 +0300

    Added BullMQ queue definitions

---
+++ backend/redis/connection.ts
@@ -0,0 +1,6 @@
+export const connection = {
+    host: process.env.REDIS_HOST || "localhost",
+    port: parseInt(process.env.REDIS_PORT || "6379"),
+    username: process.env.REDIS_USERNAME || "default",
+    password: process.env.REDIS_PASSWORD || undefined,
+}
